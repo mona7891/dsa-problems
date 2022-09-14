fdcf6e02942241c8c2d20ae5a2853e40aca72844
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-09-14 20:19:37 +0530

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,19 @@
+;; #include<stdio.h>
+;; void main()
+;; 	{
+;;	  printf("Hello World");
+;;	}
+
+
+section .data
+	msg db "Hello World",10,0
+section .text
+	global main
+	extern printf
+main:
+ 
+	push msg 
+	call printf
+	add esp,4
+	
+ret
