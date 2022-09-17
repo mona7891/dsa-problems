4b94e1feb30fef9578d99c1c79385f5df0d29546
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-09-17 15:23:47 +0530

    Add files via upload

---
+++ string_copy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+	int main()
+	{
+		char str1[20] = "Sakshi More ";
+		char str2[20];
+
+		strcpy(str2,str1);
+
+		puts(str2);                  //used to write a line or string to the output( stdout ) stream
+
+		return 0;
+
+	}
+
