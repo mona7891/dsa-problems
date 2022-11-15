3dc4e18706af25ffd8043028022b08dd53060d36
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-11-15 21:28:50 +0530

    Add files via upload

---
+++ min_array.asm
@@ -0,0 +1,70 @@
+section .data
+	print db "Enter the elements ",0
+	scan db "%d",0
+
+	printArr db "Min of Array Are : %d ",10,0
+section .bss
+	array resd 10
+	n resd 1
+section .text
+	global main
+	extern printf,scanf
+main:
+	pusha
+	push print
+	call printf
+	add esp,4
+	popa
+
+	pusha
+	push n
+	push scan
+	call scanf
+	add esp,8
+	popa
+
+	xor ecx,ecx
+lp:
+	mov ebx,array
+	mov eax,4
+
+	mul ecx
+	add ebx,eax
+
+	pusha
+	push ebx
+	push scan
+	call scanf
+	add esp,8
+	popa
+
+	inc ecx
+	cmp ecx,dword[n]
+	jl lp
+
+	xor ecx,ecx
+	mov esi,dword[array]
+lp1:
+	mov ebx,array
+	mov eax,4
+
+	mul ecx
+	add ebx,eax
+
+	cmp dword[ebx],esi
+	jnl noupdate
+
+	mov esi,dword[ebx]
+noupdate:
+	inc ecx
+	cmp ecx,dword[n]
+	jl lp1
+endof:
+	pusha
+	push esi
+	push printArr
+	call printf
+	add esp,8
+	popa
+ret
+
