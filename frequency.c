582d267ab81dec3abcef33863968ff8ca74f3d07
mona7891 <113427770+mona7891@users.noreply.github.com>
2022-11-17 20:56:17 +0530

    Add files via upload

---
+++ frequency.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	char str[100],ch;
+	int count = 0;
+
+	printf("Enter the string : ");
+	gets(str);
+
+	printf("Enter a chacter to find its frequency :");
+	scanf("%c",&ch);
+
+	for(int i=0;str[i]!='\0';i++)
+	{
+		if(ch == str[i])
+		++count;
+	}
+	printf("Freq of character %c : %d \n",ch,count);
+
+	return 0;
+	
+}
